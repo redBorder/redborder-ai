6a3235d429dbd2925b1328d7f1c0db635574e582
Pablo Pérez <pperez@redborder.com>
2024-07-23 11:33:34 +0100

    Created script

---
+++ resources/bin/rb_ai.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script that execute the AI Llamafile to generate a title of given policies. 
+# 
+# Execution:
+#     $ rb_ai.sh --options
+
+# Verify if there are params
+if [ "$#" -eq 0 ]; then
+  echo "Error: No arguments provided."
+  echo -e "Usage: ./rb_ai.sh --options...\nExecutes the AI model.\nThe model used is Llamafile"
+  exit 1
+fi
+
+/usr/lib/redborder/bin/llava-v1.5-7b-q4.llamafile "$@"
